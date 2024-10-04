d16c32731885df3dc8e20987256defb924e7c04d
이다은 <eun000221@gmail.com>
2024-10-05 01:15:10 +0900

    CH9완료, CH10 ing

---
+++ CHATPER10_Graph.py
@@ -0,0 +1,16 @@
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+def dfs(graph, start, visited = set()):
+    if start not in visited:
+        visited.add(start)
+        print(start, end=' ')
+        nbr = graph[start] - visited
+        for v in nbr:
+            dfs(graph, v, visited)

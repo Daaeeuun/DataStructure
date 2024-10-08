58e1767baee8f1f67322b1108cacae15e994cb25
이다은 <eun000221@gmail.com>
2024-10-09 01:12:04 +0900

    CH3 2회독

--- CHAPTER03_List.py
+++ CHAPTER03_List.py
@@ -148,3 +148,44 @@ setA.delete('손수건')
 setA.delete('발수건')
 setA.display('setA: ')
 setB.display('setB: ')
+
+#3.4 리스트의 응용: 라인편집기
+def myLineEditer():
+    list = ArrayList() #리스트 객체 생성
+    while True:
+        command = input("[메뉴선택] i-입력, d-삭제, r-변경, p-출력, l-파일읽기, s-저장, q-종료 => ")
+        if command == 'i':
+            pos = int(input("입력행 번호: "))
+            str = input("입력행 내용: ")
+            list.insert(pos, str)
+        elif command == 'd':
+            pos = int(input("삭제행 번호: "))
+            list.delete(pos)
+        elif command == 'r':
+            pos = int(input("변경행 번호: "))
+            str = input("변경행 내용: ")
+            list.replace(pos, str)
+        elif command == 'p':
+            # list.display()
+            print('Line Editor')
+            for line in range (list.size()):    #모든 라인에 대해
+                print('[%2d]' %line, end='')    #라인 번호와
+                print(list.getEntry(line))      #행 내용 출력
+            print()
+        elif command == 'q': return
+        elif command == 'l':
+            filename = 'test.txt'
+            infile = open(filename, "r")        #파일열기(읽기 모드)
+            lines = infile.readlines()          #파일의 모든 내용을 읽음
+            for line in lines:                  #읽혀진 각 행에 대해
+                list.insert(list.size(), line.rstrip('\n')) #
+            infile.close                        #파일 닫기
+        elif command == 's':
+            filename = 'text.txt'
+            outfile = open(filename, "w")
+            for i in range(list.size()):
+                outfile.write(list.getEntry(i)+'\n')
+            outfile.close()
+myLineEditer()
+
+        
